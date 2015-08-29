6ecb975dd99e4f5ce6217ceeb1d9004f2d6248ab
shagun11 <shagun11@users.noreply.github.com>
2015-08-30 01:28:12 +0530

    Update demo-service.js

--- sample-services/demo-service.js
+++ sample-services/demo-service.js
@@ -2,40 +2,5 @@
 angular.module('DemoServices')
 
 .factory('AppPreferences', function() {
-  var //
-  preferences = {},
 
-  setPreferences = function(obj) {
-    angular.forEach(obj, function(each) {
-      var value = '';
-      try {
-        value = angular.fromJson(each.value);
-      } catch(err) {
-        value = each.value;
-      }
-
-      preferences[each.key] = value;
-    });
-  },
-
-  getPreferences = function() {
-    return preferences;
-  };
-
-  return {
-    setPreferences: setPreferences,
-    getPreferences: getPreferences
-  };
-})
-
-//Constants set by user.
-/* FIXME: Separate user preferences from app settings */
-.constant('PREFERENCES', {
-  'correctEventTypes': ['SHIFT'],
-  'locationTypes': ['STATION'],
-  'participantTypes': ['PERSON'],
-  'eventColorTypes': ['SHIFT-COLOR', 'GIVER-COLOR'],
-  'trimmedEvents': ['SHIFT'],
-  'batchReqBaseUrl': '/api/v1/',
-  's3ImageUrl': 'https://s3.amazonaws.com/calendar-data/'
 });
